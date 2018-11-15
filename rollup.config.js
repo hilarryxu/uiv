e3306513c500b18bbc0db0ee93a8acd7bd0f2618
Larry Xu <hilarryxu@gmail.com>
2018-11-15 18:18:50 +0800

    Add build.ninja

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import pkg from './package.json'
+import vue from 'rollup-plugin-vue'
+import npm from 'rollup-plugin-node-resolve'
+import buble from 'rollup-plugin-buble'
+import { uglify } from 'rollup-plugin-uglify'
+import { minify } from 'uglify-es'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: pkg.main,
+    format: 'umd',
+    name: 'nui'
+  },
+  plugins: [
+    vue(),
+    npm({
+      jsnext: true,  // if provided in ES6
+      main: true,  // if provided in CommonJS
+      browser: true  // if provided for browsers
+    }),
+    buble(),
+    uglify({}, minify)
+  ]
+}
